caaeda1235ee1264deecd3a99dfb5a0abb07e795
Aleksandr Gridnev <gridnev.alexandr@gmail.com>
2022-11-09 17:05:21 +0400

    Make location even more wide

--- new_video_request.sh
+++ new_video_request.sh
@@ -2,4 +2,4 @@
 
 start_time=`date +%s`;
 end_time=$(($start_time + 3600));
-curl -v -F lat=41.707$(($RANDOM % 10000)) -F long=44.762$(($RANDOM % 10000)) -F start=$start_time -F end=$end_time -F direction=$(($RANDOM % 360)) -F reward=1000000000000000 -F radius=10 -F id=$start_time http://127.0.0.1:8000/internal/request/
+curl -v -F lat=41.70$(($RANDOM % 100000)) -F long=44.76$(($RANDOM % 100000)) -F start=$start_time -F end=$end_time -F direction=$(($RANDOM % 360)) -F reward=1000000000000000 -F radius=10 -F id=$start_time http://127.0.0.1:8000/internal/request/
