ec584f875f821a3c72222d45844d3f37b264849d
Aleksandr Gridnev <gridnev.alexandr@gmail.com>
2022-11-09 17:01:49 +0400

    Upgrade video creation script

--- new_video_request.sh
+++ new_video_request.sh
@@ -2,4 +2,4 @@
 
 start_time=`date +%s`;
 end_time=$(($start_time + 3600));
-curl -v -F lat=41.7078004 -F long=44.7628534 -F start=$start_time -F end=$end_time -F direction=45 -F reward=1000000000000000 -F radius=10 -F id=$start_time http://127.0.0.1:8000/internal/request/
+curl -v -F lat=41.7078$(($RANDOM % 1000)) -F long=44.7628$(($RANDOM % 1000)) -F start=$start_time -F end=$end_time -F direction=$(($RANDOM % 360)) -F reward=1000000000000000 -F radius=10 -F id=$start_time http://127.0.0.1:8000/internal/request/
