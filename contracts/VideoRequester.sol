5ccacffe4e252471063d06fee97be9876344c643
Aleksandr Gridnev <gridnev.alexandr@gmail.com>
2022-11-14 19:05:11 +0400

    Bytes based chainlink communication

--- contracts/VideoRequester.sol
+++ contracts/VideoRequester.sol
@@ -12,64 +12,89 @@ contract VideoRequester is ChainlinkClient, ConfirmedOwner {
 
     struct VideoRequest { 
         string request_id;
+        address requester;
         uint32 lat;
         uint32 long;
         uint32 start;
-        uint32 stop;
+        uint32 end;
         uint16 direction;
         uint reward;
-        string url;
-        address requester;
+        string cid;
+        address uploader;
     }
 
-    event VideoRequested(address requester, uint32 lat, uint32 long, uint32 start, uint32 end, uint16 direction, uint reward);
-    event VideoReceived(string url);
+    event VideoRequested(
+        string request_id,
+        address requester,
+        uint32 lat,
+        uint32 long,
+        uint32 start,
+        uint32 end,
+        uint16 direction,
+        uint reward
+    );
+    event VideoReceived(
+        string request_id,
+        string cid,
+        address uploader
+    );
 
     mapping (string => VideoRequest) requests;
+    mapping (bytes32 => string) chainlinkIdToRequestId;
  
     constructor() ConfirmedOwner(msg.sender) {
+        // Goerli testnet
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
         setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
     
-        jobId = 'ca98366cc7314957b8c012c72f05aeeb';
+        // source of job id: https://docs.chain.link/docs/any-api/testnet-oracles/
+        jobId = '7da2702f37fd48e5b1b9a5715e3509b6'; // bytes job
         fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
     }
- 
-    function submitRequest (string memory id, uint32 lat, uint32 long, uint32 start, uint32 end, uint16 direction) payable external {
-        emit VideoRequested(msg.sender, lat, long, start, end, direction, msg.value);
-    
-        requests[id] = VideoRequest(id, lat, long, start, end, direction, msg.value, "", msg.sender);
+
+    function submitRequest(string memory id, uint32 lat, uint32 long, uint32 start, uint32 end, uint16 direction) payable external {
+        // TODO replace id with tx hash for release version.
+        requests[id] = VideoRequest(id, msg.sender, lat, long, start, end, direction, msg.value, "", address(0));
+
+        emit VideoRequested(id, msg.sender, lat, long, start, end, direction, msg.value);
     }
 
-    function checkRequest(string memory id) public {
-        VideoRequest storage request = requests[id];
-
+    function checkRequest(string calldata id) public {
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfillVideoRequest.selector);
 
-        string memory url = string.concat("https://635bc558aa7c3f113dc5c143.mockapi.io/api/v1/videos/", request.request_id);
+        string memory url = string.concat("https://api.objective.camera/video/", id);
 
-        req.add('getURL', url);
-        req.add('pathURL', 'url');
+        req.add('get', url);
+        req.add('path', 'abi');
 
-        req.add('getID', url);
-        req.add('pathID', 'id');
+        bytes32 requestId = sendChainlinkRequest(req, fee);
 
-        sendChainlinkRequest(req, fee);
+        chainlinkIdToRequestId[requestId] = id;
     }
 
     function fulfillVideoRequest(
-        bytes32 requestId,
-        string memory urlResponse,
-        string memory idResponse
-    ) public recordChainlinkFulfillment(requestId) {
-        VideoRequest storage request = requests[idResponse];
-        request.url = urlResponse;
+        bytes32 _requestId,
+        bytes calldata data
+    ) public recordChainlinkFulfillment(_requestId) {
+        string memory videoRequestId = chainlinkIdToRequestId[_requestId];
+        VideoRequest storage videoRequest = requests[videoRequestId];
 
-        emit VideoReceived(request.url);
+        (videoRequest.uploader, videoRequest.cid) = decode(data);
+
+        emit VideoReceived(videoRequest.request_id, videoRequest.cid, videoRequest.uploader);
     }
 
-    function getUrl(string memory id) public view returns (string memory url) {
+    function getCid(string memory id) public view returns (string memory cid) {
         VideoRequest memory request = requests[id];
-        url = request.url;
+        cid = request.cid;
+    }
+
+    // example of encoding to abi
+    function encode(address addr, string memory cid) public pure returns (bytes memory) {
+        return (abi.encode(addr, cid));
+    }
+
+    function decode(bytes memory data) public pure returns (address addr, string memory cid) {
+        (addr, cid) = abi.decode(data, (address, string));
     }
 }
